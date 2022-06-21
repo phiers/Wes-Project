7bb012dc634c9fc6de80f5312091f43584aad196
Paul Hiers <paulhiers@gmail.com>
2022-06-21 13:50:03 -0400

    Change to acccomodate new file from Wes

--- scrape_census_geocoder.py
+++ scrape_census_geocoder.py
@@ -3,41 +3,47 @@ import requests
 import csv
 import time
 
-#start
+
 def main():
     print('Started...')
     startTime = time.time()
     # open source file to generate urls
-    input_file = 'shootings data test copy.csv'
+    input_file = 'Wes Project/new data file.csv'
+    # 2018 is vintage=418; 2010 is 410
+    vintage = 418
+    output_file = 'results_wp_2018.csv' #rename for each iteration to create 4 different files
     # write headers to result file outside of loop
-    with open('results.csv', mode='w') as results:
-                fieldnames = ['id','longitude_wp', 'latitude_wp','cousubgeoid_wp', 'cousubname_wp', 'tractgeoid_wp', 'tractcode_wp','tractname_wp']
-                writer = csv.DictWriter(results, fieldnames)
-                writer.writeheader()
+    with open(output_file, mode='w') as results:
+        fieldnames = ['id', 'longitude_wp', 'latitude_wp', 'cousubgeoid_wp',
+                      'cousubname_wp', 'tractgeoid_wp', 'tractcode_wp', 'tractname_wp']
+        writer = csv.DictWriter(results, fieldnames)
+        writer.writeheader()
 
     with open(input_file) as csvfile:
         file = csv.DictReader(csvfile)
         for row in file:
             long = row['longitude_wp']
             lat = row['latitude_wp']
-            row_id = row['id']
+            row_id = row['\ufeffwapo_id']
             if lat and long:
-                url = f"https://geocoding.geo.census.gov/geocoder/geographies/coordinates?x={long}&y={lat}&benchmark=4&vintage=418"
-       
+                url = f"https://geocoding.geo.census.gov/geocoder/geographies/coordinates?x={long}&y={lat}&benchmark=4&vintage={vintage}"
+
                 # within the loop, get the soup and return the values from the webpage needed
                 x = get_soup(url)
                 y = get_data(x, row_id, long, lat)
-                print(y)
-                #write the new values to a csv file
-                with open('results.csv', mode='a') as results:
-                    fieldnames = ['id','longitude_wp', 'latitude_wp','cousubgeoid_wp', 'cousubname_wp', 'tractgeoid_wp', 'tractcode_wp','tractname_wp']
+
+                # write the new values to a csv file
+                with open(output_file, mode='a') as results:
+                    fieldnames = ['id', 'longitude_wp', 'latitude_wp', 'cousubgeoid_wp',
+                                  'cousubname_wp', 'tractgeoid_wp', 'tractcode_wp', 'tractname_wp']
                     writer = csv.DictWriter(results, fieldnames)
                     writer.writerow(y)
             else:
                 pass
-    
+
     endTime = time.time()
     print(f'The program took {(endTime-startTime)/60} minutes to complete')
+
 
 def get_soup(url):
     headers = {
@@ -51,16 +57,17 @@ def get_soup(url):
     }
     ret = requests.get(url, headers=headers)
     page_soup = soup(ret.content, 'html.parser')
-    good_soup = page_soup.select('fieldset div')  
-    good_soup = good_soup.pop() # the div we want is the last one
-    good_soup = list(good_soup.strings) #change to strings so I can work with the output
+    good_soup = page_soup.select('fieldset div')
+    good_soup = good_soup.pop()  # the div we want is the last one
+    # change to strings so I can work with the output
+    good_soup = list(good_soup.strings)
 
     return good_soup
 
 
 def get_data(soup, id, long, lat):
     result_list = []
-    #adding these to dict here, but could do it in main
+    # adding these to dict here, but could do it in main
     result_dict = {'id': id, 'longitude_wp': long, 'latitude_wp': lat}
     # create list from soup
     for i, x in enumerate(soup):
@@ -72,19 +79,18 @@ def get_data(soup, id, long, lat):
             result_list.append(soup[i + 1])
         if 'TRACT CODE' in x:
             result_list.append(soup[i + 1])
-    print(result_list)
+    
     # narrow list down to those we need
     for ind, item in enumerate(result_list):
         if 'County Subd' in item:
-            result_dict.update({'cousubgeoid_wp' : result_list[ind+1]})
-            result_dict.update({'cousubname_wp' : result_list[ind+2]})
+            result_dict.update({'cousubgeoid_wp': result_list[ind+1]})
+            result_dict.update({'cousubname_wp': result_list[ind+2]})
         if 'Census Tracts' in item:
-            result_dict.update({'tractgeoid_wp' : result_list[ind+1]})
-            result_dict.update({'tractcode_wp' : result_list[ind+2]})
-            result_dict.update({'tractname_wp' : result_list[ind+3]})
+            result_dict.update({'tractgeoid_wp': result_list[ind+1]})
+            result_dict.update({'tractcode_wp': result_list[ind+2]})
+            result_dict.update({'tractname_wp': result_list[ind+3]})
     return result_dict
 
 
 if __name__ == "__main__":
     main()
-
